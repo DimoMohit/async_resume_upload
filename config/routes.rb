5407d23fa4424eb2913df78b2c976b0365285761
Mohit <mohitdutta@live.com>
2014-11-04 14:41:57 +0530

    Gemfile for Rails3

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ FormTagDemo::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'login#index'
-
+  #root 'login#index'
+  get '/'=> 'login#index'
   post '/upload' => 'login#create',:defaults => { :format => 'js' }
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
